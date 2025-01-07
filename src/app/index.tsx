deec44834f0a64ed58f3e24cfbe88b9735bfcbb4
Shane <shaneedelstein@gmail.com>
2025-01-07 14:02:09 -0500

    first commit

---
+++ src/app/index.tsx
@@ -0,0 +1,25 @@
+import { Link } from 'expo-router';
+import { Text } from 'react-native';
+
+import { Button } from '~/components/Button';
+import { Container } from '~/components/Container';
+
+export default function StartupMenu() {
+  return (
+    <Container>
+      <Text className={styles.title}>Welcome to MinMaxLife!</Text>
+      <Text className={styles.subtitle}>Choose an option to get started:</Text>
+      <Link href="/details" asChild>
+        <Button title="View Details" />
+      </Link>
+      <Link href="/" asChild>
+        <Button title="Home" />
+      </Link>
+    </Container>
+  );
+}
+
+const styles = {
+  title: 'text-2xl font-bold text-center mb-4',
+  subtitle: 'text-lg text-center mb-6',
+};
--- src/utils/supabase.ts
+++ src/utils/supabase.ts
@@ -1,8 +1,9 @@
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = process.env.EXPO_PUBLIC_SUPABASE_URL;
-const supabaseAnonKey = process.env.EXPO_PUBLIC_SUPABASE_ANON_KEY;
+const supabaseUrl = 'https://fvkbzmenfqvbbhcxsjaw.supabase.co';
+const supabaseAnonKey =
+  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZ2a2J6bWVuZnF2YmJoY3hzamF3Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzYyNzUwNTMsImV4cCI6MjA1MTg1MTA1M30.HHtu6HLhBOKh0mrhFU-iJoAbFWSiM0hmjuEXhHX3VKA';
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey, {
   auth: {
